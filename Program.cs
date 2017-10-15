a326eed4c4a78336258004535dbf48eea750e469
Ying Xu <xuyying@gmail.com>
2017-10-14 21:51:55 -0700

    C# test code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+
+            Task t1 = FooAsync("t1");
+            Task t2 = FooAsync("t2");
+            Task.WaitAll(t1, t2);
+            
+            sw.Stop();
+            Console.WriteLine($"Wait time: {sw.ElapsedMilliseconds}");
+        }
+
+        static async Task FooAsync(string s)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                await Task.Delay(i);
+                Console.WriteLine($"{s} - {i}");
+            }
+        }
+    }
+}
